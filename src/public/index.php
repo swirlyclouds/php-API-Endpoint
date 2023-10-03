28290a6b7d7b498c03cac0f802e20df1eacc13d2
Swirly Clouds <muazatif@protonmail.com>
2023-10-03 22:41:16 +0100

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,18 @@
 <?php
+$con = mysqli_connect('db','root','root');
 
+mysqli_select_db($con,"services_db");
+$sql = "SELECT * FROM services";
+$result =  mysqli_query($con,$sql);
+
+$payload = [];
+
+while($row = mysqli_fetch_array($result)) {
+    $data = array("Ref"=>$row['Ref'], "Centre" => $row['Centre'], "Service" => $row['Service'], "Country" => $row['Country']);
+    header("Content-Type: application/json");
+    $payload[] = $data;
+}
+
+echo json_encode($payload);
+mysqli_close($con);
+exit();
